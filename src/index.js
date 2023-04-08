46a0538d3c98028ca483711b4cacd7e210f158d7
Pulkit Sharma <91002702+Pulkit-007@users.noreply.github.com>
2023-04-08 14:44:30 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom"; //new way
+ReactDOM.render(
+  <div>
+    <h1>Hello World!!</h1>,<p> This is a paragraph guyysss</p>
+  </div>,
+  document.getElementById("root")
+);
+
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hello World!!";
+// document.getElementById("root").appendChild(h1);
